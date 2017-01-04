1965d41335bfb0ee2a2ed28af998f354a3b3b3cd
Abbondanzo <peter@abbondanzo.com>
2017-01-04 08:25:19 -0500

    Comments

--- js/popup.js
+++ js/popup.js
@@ -6,14 +6,17 @@ function closeAll() {
     // Reveal mobile menu close button
     $('.close-menu').css('display','block');
 }
+// Close when hitting ESC key
 $(document).keyup(function(e) {
      if (e.keyCode == 27) { // escape key maps to keycode 27
         closeAll();
     }
 });
+// Close "X" button
 $('.contact-close').one('click',function() {
     closeAll();
 });
+// Close when clicking outside of window space
 $(document).one('click',function() {
     if(!$(event.target).closest('.item-view').length) {
         closeAll();
