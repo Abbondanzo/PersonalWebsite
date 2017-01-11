062b63002d492cf80ed2df413b365665d2bd50e9
Abbondanzo <peter@abbondanzo.com>
2017-01-11 00:14:20 -0500

    Added Rogue images

--- rogue.php
+++ rogue.php
@@ -6,11 +6,13 @@
                 <span class="underscore-pls"></span>
                 <p>Rogue was designed as a simple one-page demo of a client-side application for the famous Counter Strike: Global Offensive game. The client wanted an effective means of showcasing the power of their application, so a full-page live video runs above the fold for every user that reaches the site.</p>
             </left><right>
+                <img style="float: right;" height="318px" alt="Preview of Rogue on a Macbook Pro laptop" src="img/660pxrogue1.png">
             </right>
 
         </div>
         <div>
             <left>
+                <img alt="Second half of Rogue web page" src="img/660pxrogue2.png">
             </left><right class="lr-content">
                 <p>Each user that came across the page could view the primary features of the application and request a license with a simple one-click checkout. The footer kept track of the hundreds of users who had installed the application, and allowed new buyers to email their questions to the client.</p>
                 <a target="_blank" href="http://demo.abbondanzo.com/rogue/"><button class="btn">View Demo</button></a>
