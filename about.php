708d6414b8c6547975c2ab4383f2b083b93222e0
Abbondanzo <peter@abbondanzo.com>
2017-02-01 19:01:36 -0500

    New images + css inclusion

--- about.php
+++ about.php
@@ -2,18 +2,14 @@
 <html lang="en-us">
 <!-- Code solely created by Peter V. Abbondanzo. Copyright 2017. All rights reserved. -->
 <head>
-	<title>Peter V. Abbondanzo | Web &amp; Mobile Developer</title>
+	<title>About Me | Peter V. Abbondanzo</title>
 	<?php require_once 'head.php'; ?>
 </head>
-<body class="landing">
+<body>
 	<?php require_once 'navbar.php'; ?>
 	<div class="content">
 		<div class="container valign">
-			<h1>Hello,</h1>
-			<h1>You've found me.</h1>
-			<a href="/about">
-				<button class="btn btn-white">About</button>
-			</a>
+			
 		</div>
 	</div>
 </body>
---
+++ navbar.php
@@ -0,0 +1,10 @@
+<nav class="nav">
+    <div class="nav-logo"></div>
+    <div class="nav-links">
+        <ul>
+            <li><a class="underline" href="/about">About</a></li>
+            <li><a class="underline" href="/projects">Projects</a></li>
+            <li><a class="underline" href="#">Contact</a></li>
+        </ul>
+    </div>
+</nav>
