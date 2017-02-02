8b1b51c4e389798617f1d7b4d0fcf5694c5a4c7a
Abbondanzo <peter@abbondanzo.com>
2017-02-02 03:19:48 -0500

    Text animation--needs update case for span across multiple lines

--- js/script.js
+++ js/script.js
@@ -1,15 +1,35 @@
 // Animations "Hello, you found me." intro
-function animHello() {
-    $('.hello-text h1').each(function() {
-        var i = 0;
-        var content = $(this).html();
+function animHello(index) {
+    $html = $('.hello-text h1').eq(index);
+    var content = $html.html();
+    if(content != undefined) {
         var length = content.length;
-        while (i < length) {
-            setTimeout(function() {
-                $(this).html('<span>'+content.slice(0, i)+'</span>'+content.slice(i, length));
-            }, 10000);
-            i++;
-        }
-    })
+        $('span.blink').remove();
+        $html.addClass('active-text');
+        showWord(content, length, 1, $html);
+    }
 }
-animHello();
+
+// Animates a given word of given length inside given html section
+function showWord(word, length, count, $html) {
+    $html.html('<span>'+word.slice(0, count)+'</span>'+word.slice(count, length)+'<span class="blink">|</span>');
+    var margin = $('.active-text span').width();
+    var width = $html.width();
+    console.log(width);
+    $('.blink').css('margin-left', margin - width + 27);
+    if(count < length) {
+        setTimeout(function() {
+            showWord(word, length, count+1, $html);
+            //console.log(word,length,count);
+        }, 100);
+    } else {
+        setTimeout(function() {
+            $html.removeClass('active-text');
+            animHello($html.index()+1);
+        }, 1000);
+    }
+}
+
+setTimeout(function() {
+    animHello(0);
+}, 1000);
