afcd1addf9f365316b87a607de7449528118bdbb
Abbondanzo <peter@abbondanzo.com>
2017-02-26 06:09:36 -0500

    Homepage edit

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -13,42 +13,9 @@
 </template>
 
 <script>
-
-// Animations "Hello, you found me." intro
-function animHello (index) {
-    var html = $('.hello-text h1').eq(index);
-    var content = html.html();
-    if(content != undefined) {
-        var length = content.length;
-        $('span.blink').remove();
-        html.addClass('active-text');
-        showWord(content, length, 1, $html);
-    }
+export default {
+    name: 'Home'
 }
-
-// Animates a given word of given length inside given html section
-function showWord (word, length, count, $html) {
-    $html.html('<span>'+word.slice(0, count)+'</span>'+word.slice(count, length)+'<span class="blink">|</span>');
-    var margin = $('.active-text span').width();
-    var width = $html.width();
-    console.log(width);
-    //$('.blink').css('margin-left', margin - width + 27);
-    if(count < length) {
-        setTimeout(function() {
-            showWord(word, length, count+1, $html);
-        }, 80);
-    } else {
-        setTimeout(function() {
-            $html.removeClass('active-text');
-            animHello($html.index()+1);
-        }, 1000);
-    }
-}
-
-// Start landing animation
-setTimeout(function() {
-    animHello(0);
-}, 1000);
 </script>
 
 <style lang="scss">
