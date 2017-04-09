5f3ded20ed474b76f94db3b564c10fe57a0e0b49
Abbondanzo <peter@abbondanzo.com>
2017-04-08 21:26:48 -0400

    Slight UX fixes, removing old scripts

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -21,37 +21,6 @@ export default {
         console.log('Mounted Home component')
     }
 }
-
-/* function showWord (word, length, count, $html) {
-    $html.innerHTML = '<span>' + word.slice(0, count) + '</span>' + word.slice(count, length) + '<span class="blink">|</span>'
-    $html.querySelector('span').style.visibility = 'visible'
-    // var margin = document.querySelector('.active-text').offsetWidth
-    // var width = $html.offsetWidth
-    // document.querySelector('.blink').style.marginLeft = margin - width + 27
-    if (count < length) {
-        setTimeout(function () {
-            showWord(word, length, count + 1, $html)
-        }, 80)
-    } else {
-        setTimeout(function () {
-            $html.classList.remove('active-text')
-            // animHello(1)
-            console.log(document.querySelectorAll('.hello-text h1').indexOf($html.outerHTML))
-        }, 1000)
-    }
-}
-
-function animHello (index) {
-    var $html = document.querySelectorAll('.hello-text h1')[index]
-    var content = $html.textContent
-    if (content !== undefined) {
-        var length = content.length
-        // document.querySelector('span.blink').remove()
-        $html.className += 'active-text'
-        showWord(content, length, 1, $html)
-    }
-} */
-
 </script>
 
 <style lang="scss" scoped>
@@ -68,14 +37,16 @@ body {
         .hello-text {
             text-align: center;
             // overflow: auto;
-            h1 {
+            h1, h2 {
                 color: #fff;
-                font-size: 96px;
                 cursor: default;
+                pointer-events: none;
+            }
+            h1 {
+                font-size: 96px;
                 // white-space: nowrap;
             }
             h2 {
-                color: #fff;
                 font-weight: 300;
                 font-size: 40px;
                 padding-bottom: 48px;
