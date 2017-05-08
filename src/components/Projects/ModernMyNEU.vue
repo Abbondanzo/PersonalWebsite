7754437ed4fa7843fd9c71d8a914169989416215
Abbondanzo <peter@abbondanzo.com>
2017-05-07 23:52:47 -0400

    Fixed verbiage

--- src/components/Projects/ModernMyNEU.vue
+++ src/components/Projects/ModernMyNEU.vue
@@ -7,7 +7,7 @@
             </div>
         </div>
         <div id="text" class="p-text">
-            <p>Modern MyNEU is a fully functional Google Chrome extension, available in the webstore, that completely redesigns Northeastern University's outdated and ill-designed MyNEU portal.</p>
+            <p>Modern MyNEU is a fully functional Google Chrome extension, available in the webstore, that completely redesigns Northeastern University's outdated and ill-designed MyNEU portal. Originally designed as a weekend project, this extension has evolved into a widely-used application across campus, inspiring both students and administration at Northeastern to support the reconstruction of a new, user-friendly portal.</p>
             <p>Large amounts of features and reimplementations of the old site have been added to the extension. A freshly designed login portal allows users to take advantage of password storage apps where the old site would prevent. A search bar that narrows down specific widgets within the portal has also been added. Enhanced drop-down menus add to the capabilities that the vanilla site is unable to provide.</p>
             <p>Since the plugin is still in beta stages, new features are getting added that will allow users to customize layouts and themes from within the options pop-out. Everything down to the individual colors of elements and the ability to export custom preferences will give users the full control they desire from MyNEU.</p>
             <a href="https://chrome.google.com/webstore/detail/modern-myneu/phpjhjbppfdgofldhpecojjdfccboblk">
