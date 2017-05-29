0c38112c52775a273c581e8c2974f162b815c3eb
Abbondanzo <peter@abbondanzo.com>
2017-05-29 13:31:51 -0400

    Fixed slider animations

--- src/components/ImageSlider.vue
+++ src/components/ImageSlider.vue
@@ -1,9 +1,12 @@
 <template>
     <div class="image-slider" @mousedown="startTrack" @mousemove="track" @mouseup="endTrack" @mouseleave="mouseDown=false">
-        <div class="image"
-        v-for="(image, index) in images"
-        v-bind:index="index"
-        v-bind:style="{ 'background-image': 'url(' + images[index] + ')' }">
+        <div class="image-wrapper">
+            <div class="image"
+            v-for="(image, index) in images"
+            v-bind:index="index"
+            v-bind:style="{ 'background-image': 'url(' + images[index] + ')',
+            'transform': 'translateX(' + (index * 100) + '%)' }">
+            </div>
         </div>
         <i class="fa fa-chevron-left" aria-hidden="true" v-on:click="moveRight"></i>
         <i class="fa fa-chevron-right" aria-hidden="true" v-on:click="moveLeft"></i>
@@ -18,7 +21,7 @@ export default {
             active: 0,
             cursorPos: 0,
             mouseDown: false,
-            imagePos: []
+            imagePos: 0
         }
     },
     props: {
@@ -39,24 +42,12 @@ export default {
         track: function (e) {
             if (this.mouseDown) {
                 var sliderWidth = document.querySelector('.image-slider').offsetWidth
-                var images = document.querySelectorAll('.image-slider .image')
-                var pos = this.imagePos
+                var wrapper = document.querySelector('.image-wrapper')
                 var cursor = this.cursorPos
-                if (pos.length > 0) {
-                    var current = this.active
-                    images.forEach(function (img) {
-                        var idx = img.getAttribute('index')
-                        var transform = parseFloat(pos[idx])
-                        // Percentage-based change
-                        var change = (100 * (e.clientX - cursor) / sliderWidth)
-                        img.style.transform = 'translateX(' + (change + transform) + '%)'
-                        // Set further image to proper position
-                        var farLeftImage = images[(current - 2 + images.length) % images.length]
-                        farLeftImage.style.transform = 'translateX(' + (-200 + change) + '%)'
-                        var farRightImage = images[(current + 2) % images.length]
-                        farRightImage.style.transform = 'translateX(' + (200 + change) + '%)'
-                    })
-                }
+                // Percentage-based change
+                var change = (100 * (e.clientX - cursor) / sliderWidth)
+                var currentPos = parseFloat(this.imagePos)
+                wrapper.style.transform = 'translateX(' + (change + currentPos) + '%)'
             }
         },
         startTrack: function (e) {
@@ -79,48 +70,25 @@ export default {
             this.mouseDown = false
         },
         setState: function () {
-            // Stored as percentages
-            var positions = []
-            var images = document.querySelectorAll('.image-slider .image')
-            images.forEach(function (img) {
-                var transform
-                if (img.style.transform) {
-                    transform = img.style.transform.split(/[()]/)[1]
-                } else {
-                    transform = 0
-                }
-                positions.push(transform)
-            })
-            this.imagePos = positions
+            // Stored as percentage
+            var wrapper = document.querySelector('.image-wrapper')
+            var position = wrapper.style.transform.split(/[(%)]/)[1]
+            if (!position) {
+                position = 0
+            }
+            this.imagePos = position
         },
         shiftPosition: function () {
-            var images = document.querySelectorAll('.image-slider .image')
+            this.updateActive()
+            var wrapper = document.querySelector('.image-wrapper')
+            wrapper.className += ' animated'
+            var shift = -(this.active / 2) * 100
             // "Hide" all images
-            images.forEach(function (img) {
-                img.style.transform = 'translateX(-200%)'
-                img.style.opacity = '0.5'
-                img.style.zIndex = '1'
-            })
-            var left
-            var right
-            if (this.active === 0) {
-                left = images[images.length - 1]
-            } else {
-                left = images[this.active - 1]
-            }
-            if (this.active + 1 === images.length) {
-                right = images[0]
-                right.style.transform = 'translateX(200%)'
-            } else {
-                right = images[this.active + 1]
-            }
-            var animatedImages = [images[this.active], left, right]
-            this.animated(animatedImages)
-            left.style.transform = 'translateX(-100%)'
-            images[this.active].style.transform = 'translateX(0%)'
-            images[this.active].style.opacity = '1'
-            right.style.transform = 'translateX(100%)'
-            this.setState()
+            wrapper.style.transform = 'translateX(' + (shift) + '%)'
+            // TODO: Fix timing issue when multiple shifts happen in under 0.5s
+            setTimeout(function () {
+                wrapper.className = wrapper.className.split(' animated')[0]
+            }, 500)
         },
         moveLeft: function () {
             var newActive = this.active + 1
@@ -138,19 +106,22 @@ export default {
             this.active = newActive
             this.shiftPosition()
         },
-        animated: function (images) {
-            // "Hide" all images
+        updateActive: function () {
+            var images = document.querySelectorAll('.image-slider .image')
+            var current = this.active
             images.forEach(function (img) {
-                img.className += ' animated'
-                img.style.zIndex = '2'
-                setTimeout(function () {
-                    img.className = img.className.split(' animated')[0]
-                }, 1000)
+                var idx = parseInt(img.getAttribute('index'))
+                console.log(idx, current)
+                if (idx === current) {
+                    img.className = 'image active'
+                } else {
+                    img.className = 'image'
+                }
             })
         }
     },
     mounted () {
-        this.shiftPosition()
+        this.updateActive()
     }
 }
 </script>
@@ -165,16 +136,21 @@ body {
     cursor: move;
     overflow-x: hidden;
     position: relative;
+    .image-wrapper.animated {
+        @include transition(all $anim * 2 $ease-out-quint)
+    }
     .image {
         height: 450px;
         width: 50%;
         margin-left: -25%;
         background-size: cover;
+        opacity: 0.5;
         font-size: 0;
         display: inline-block;
         position: absolute;
-        &.animated {
-            @include transition(all $anim * 2 $ease-out-quint)
+        @include transition(opacity $anim * 2 $ease-out-quint)
+        &.active {
+            opacity: 1;
         }
     }
     i {
