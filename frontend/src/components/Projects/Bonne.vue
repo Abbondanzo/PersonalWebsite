b9a01ddf5139c353efced8de8708fadf63104186
Abbondanzo <peter@abbondanzo.com>
2019-07-13 13:42:19 -0400

    Use 2 width spacing

--- frontend/src/components/Projects/Bonne.vue
+++ frontend/src/components/Projects/Bonne.vue
@@ -1,51 +1,51 @@
 <template>
-    <div class="content project">
-        <div class="p-heading">
-            <div class="p-title">
-                <h1>Bonne Vie Café</h1>
-                <h4>Simple cafe web-page mockup.</h4>
-            </div>
-        </div>
-        <div id="text" class="p-text">
-            <p>Bonne Vie Café began as a mockup design made for a college student. He had very little requirements, other than to satisfy a number of sections. When he came to me, I knew just how to build the perfect site.</p>
-            <img src="@assets/img/bonne/b1.jpg">
-            <span>Stylesheet and index side-by-side</span>
-            <p>I started by designing a wireframe and various slides in Adobe Photoshop and InDesign. The customer instantly fell in love and we went from there. I built the site from scratch using various amounts of PHP and Javascript to handle contact/review forms and table reservations. Everything else was made using HTML and CSS.</p>
-            <img src="@assets/img/bonne/b2.jpg">
-            <img style="padding:40px 0;" src="@assets/img/bonne/b3.jpg">
-            <p>Users can book a reservation in just a few clicks. I utilized Zapla's table management API to allow potential restaurant-goers to see what number of tables are available. Users could also view a number of informative canvases that displayed restaurant location and contact details.</p>
-            <a href="http://cafe.abbondanzo.com/">
-                <button class="btn">View Mockup</button>
-            </a>
-        </div>
+  <div class="content project">
+    <div class="p-heading">
+      <div class="p-title">
+        <h1>Bonne Vie Café</h1>
+        <h4>Simple cafe web-page mockup.</h4>
+      </div>
     </div>
+    <div id="text" class="p-text">
+      <p>Bonne Vie Café began as a mockup design made for a college student. He had very little requirements, other than to satisfy a number of sections. When he came to me, I knew just how to build the perfect site.</p>
+      <img src="@assets/img/bonne/b1.jpg" />
+      <span>Stylesheet and index side-by-side</span>
+      <p>I started by designing a wireframe and various slides in Adobe Photoshop and InDesign. The customer instantly fell in love and we went from there. I built the site from scratch using various amounts of PHP and Javascript to handle contact/review forms and table reservations. Everything else was made using HTML and CSS.</p>
+      <img src="@assets/img/bonne/b2.jpg" />
+      <img style="padding:40px 0;" src="@assets/img/bonne/b3.jpg" />
+      <p>Users can book a reservation in just a few clicks. I utilized Zapla's table management API to allow potential restaurant-goers to see what number of tables are available. Users could also view a number of informative canvases that displayed restaurant location and contact details.</p>
+      <a href="http://cafe.abbondanzo.com/">
+        <button class="btn">View Mockup</button>
+      </a>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'bonne'
+  name: 'bonne'
 }
 </script>
 
 <style lang="scss" scoped>
 @import '~@assets/sass/variables';
 .project {
-    .p-heading {
-        background: linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.8)),
-            url('~@assets/img/bvc.jpg') no-repeat;
+  .p-heading {
+    background: linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.8)),
+      url('~@assets/img/bvc.jpg') no-repeat;
+  }
+  .p-text {
+    .block-1 {
+      width: 40%;
+      margin: 80px 20% 0 10%;
     }
-    .p-text {
-        .block-1 {
-            width: 40%;
-            margin: 80px 20% 0 10%;
-        }
-        .block-2 {
-            width: 60%;
-            margin: 100px 0 0 30%;
-        }
-        p:last-child {
-            margin-top: -300px;
-        }
+    .block-2 {
+      width: 60%;
+      margin: 100px 0 0 30%;
     }
+    p:last-child {
+      margin-top: -300px;
+    }
+  }
 }
 </style>
--- frontend/src/components/Projects/Main.vue
+++ frontend/src/components/Projects/Main.vue
@@ -1,240 +1,256 @@
 <template>
-    <div class="content">
-        <section>
-            <div class="left">
-                <div class="project-list valign">
-                    <ul>
-                        <h2>Recent Projects</h2>
-                        <li v-for="(project, index) in projects"
-                            v-bind:key="index"
-                            v-bind:index="index"
-                            v-bind:class="isActive(index)"
-                            v-on:mouseover="projectCheck(index)"
-                            v-on:click="openProject(index)">
-                            <span>0{{ index + 1 }}</span> <h3>{{ project.title }}</h3>
-                        </li>
-                    </ul>
-                </div>
-                <div class="popup info-block">
-                    <p>Want to see the code behind these projects? Check out my Github profile at <a class="under" target="_blank" title="Github Profile" href="https://github.com/Abbondanzo">this link</a>.</p>
-                </div>
-            </div>
-            <div class="right">
-                <img v-bind:class="preview" v-bind:height="previewHeight" v-bind:src="img" v-on:click="openProject(activeProject)"/>
-            </div>
-        </section>
-    </div>
+  <div class="content">
+    <section>
+      <div class="left">
+        <div class="project-list valign">
+          <ul>
+            <h2>Recent Projects</h2>
+            <li
+              v-for="(project, index) in projects"
+              v-bind:key="index"
+              v-bind:index="index"
+              v-bind:class="isActive(index)"
+              v-on:mouseover="projectCheck(index)"
+              v-on:click="openProject(index)"
+            >
+              <span>0{{ index + 1 }}</span>
+              <h3>{{ project.title }}</h3>
+            </li>
+          </ul>
+        </div>
+        <div class="popup info-block">
+          <p>
+            Want to see the code behind these projects? Check out my Github profile at
+            <a
+              class="under"
+              target="_blank"
+              title="Github Profile"
+              href="https://github.com/Abbondanzo"
+            >this link</a>.
+          </p>
+        </div>
+      </div>
+      <div class="right">
+        <img
+          v-bind:class="preview"
+          v-bind:height="previewHeight"
+          v-bind:src="img"
+          v-on:click="openProject(activeProject)"
+        />
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'projects',
-    data() {
-        return {
-            img: require('@assets/img/flipster.jpg'),
-            activeProject: 0,
-            windowHeight: 0,
-            preview: '',
-            previewHeight: 0,
-            projects: [
-                { title: 'Flipster', source: 'flipster', image: 'flipster.jpg' },
-                { title: 'Bonne Vie Cafe', source: 'bvc', image: 'bvc.jpg' },
-                { title: 'Rogue', source: 'rogue', image: 'rogue.jpg' },
-                { title: 'FeedShare', source: 'feedshare', image: 'feedshare.jpg' },
-                { title: 'Sthacks', source: 'sthacks', image: 'sth.jpg' },
-                { title: 'Modern MyNEU', source: 'myneu', image: 'myneu.jpg' }
-            ]
-        }
-    },
-    mounted() {
-        this.$nextTick(function() {
-            window.addEventListener('resize', this.getWindowHeight)
-            this.getWindowHeight()
-        })
-    },
-    methods: {
-        projectCheck(arg) {
-            this.activeProject = arg
-            let newImg = require('@assets/img/' + this.projects[arg].image)
-            this.$nextTick(function() {
-                this.img = newImg
-            })
-        },
-        isActive(arg) {
-            if (arg === this.activeProject) {
-                return {
-                    active: 'active'
-                }
-            }
-        },
-        getWindowHeight(event) {
-            this.windowHeight = document.documentElement.clientHeight
-            let left = document.querySelector('.left')
-            if (left) {
-                let leftHeight = left.clientHeight
-                if (this.windowHeight < leftHeight) {
-                    this.preview = 'contract'
-                    this.previewHeight = leftHeight
-                } else {
-                    this.preview = 'expand'
-                }
-            }
-        },
-        openProject(arg) {
-            this.$router.replace({
-                path: '/projects/' + this.projects[arg].source
-            })
-        }
+  name: 'projects',
+  data() {
+    return {
+      img: require('@assets/img/flipster.jpg'),
+      activeProject: 0,
+      windowHeight: 0,
+      preview: '',
+      previewHeight: 0,
+      projects: [
+        { title: 'Flipster', source: 'flipster', image: 'flipster.jpg' },
+        { title: 'Bonne Vie Cafe', source: 'bvc', image: 'bvc.jpg' },
+        { title: 'Rogue', source: 'rogue', image: 'rogue.jpg' },
+        { title: 'FeedShare', source: 'feedshare', image: 'feedshare.jpg' },
+        { title: 'Sthacks', source: 'sthacks', image: 'sth.jpg' },
+        { title: 'Modern MyNEU', source: 'myneu', image: 'myneu.jpg' }
+      ]
     }
+  },
+  mounted() {
+    this.$nextTick(function() {
+      window.addEventListener('resize', this.getWindowHeight)
+      this.getWindowHeight()
+    })
+  },
+  methods: {
+    projectCheck(arg) {
+      this.activeProject = arg
+      let newImg = require('@assets/img/' + this.projects[arg].image)
+      this.$nextTick(function() {
+        this.img = newImg
+      })
+    },
+    isActive(arg) {
+      if (arg === this.activeProject) {
+        return {
+          active: 'active'
+        }
+      }
+    },
+    getWindowHeight(event) {
+      this.windowHeight = document.documentElement.clientHeight
+      let left = document.querySelector('.left')
+      if (left) {
+        let leftHeight = left.clientHeight
+        if (this.windowHeight < leftHeight) {
+          this.preview = 'contract'
+          this.previewHeight = leftHeight
+        } else {
+          this.preview = 'expand'
+        }
+      }
+    },
+    openProject(arg) {
+      this.$router.replace({
+        path: '/projects/' + this.projects[arg].source
+      })
+    }
+  }
 }
 </script>
 <style lang="scss" scoped>
 @import '~@assets/sass/variables';
 .project-return {
-    display: none;
+  display: none;
 }
 .content {
-    height: 100vh;
+  height: 100vh;
 }
 section {
-    height: 100vh;
-    width: 100%;
-    overflow: hidden;
-    display: table;
-    table-layout: fixed;
+  height: 100vh;
+  width: 100%;
+  overflow: hidden;
+  display: table;
+  table-layout: fixed;
+  .left,
+  .right {
+    position: relative;
+    display: table-cell;
+    font-size: 0;
+  }
+  .left {
+    background: $bgcolor2;
+    height: 100%;
+    margin-right: $padding;
+    margin-left: -$padding;
+    padding-top: $navbarheight;
+    z-index: 2;
+    .project-list {
+      margin-right: -$padding;
+      margin-left: $padding * 2;
+      ul {
+        list-style: none;
+        padding: $padding;
+        background: #fff;
+        li {
+          cursor: pointer;
+          margin-bottom: 24px;
+          line-height: 36px;
+          margin-left: $padding / 2;
+          h3,
+          span {
+            display: inline-block;
+            vertical-align: bottom;
+          }
+          h3 {
+            font-size: 24px;
+            font-weight: 600;
+            color: $textcolor2;
+            transition: all $anim / 2 $ease-out-quint;
+          }
+          span {
+            color: $primary;
+            font-size: 14px;
+            line-height: 36px;
+          }
+          &:hover,
+          &.active {
+            h3 {
+              font-size: 32px;
+              color: $primary;
+            }
+          }
+          &:last-child {
+            margin-bottom: 0;
+          }
+        }
+      }
+    }
+  }
+  .right {
+    z-index: 1;
+    img {
+      cursor: pointer;
+      min-width: 100%;
+      &.expand {
+        height: 100vh;
+      }
+    }
+  }
+  .info-block {
+    background: $primary;
+    padding: $padding / 2;
+    position: absolute;
+    bottom: $padding / 2;
+    left: 90%;
+    min-width: 200px;
+    width: 320px;
+    p {
+      color: #fff;
+      font-size: 14px;
+    }
+    .under {
+      color: #fff;
+      &::before {
+        background: #fff;
+      }
+      &::after {
+        background: $primary;
+      }
+    }
+  }
+  @media screen and (max-width: 960px) {
     .left,
     .right {
-        position: relative;
-        display: table-cell;
-        font-size: 0;
+      width: 100%;
+      display: block;
     }
     .left {
-        background: $bgcolor2;
-        height: 100%;
-        margin-right: $padding;
-        margin-left: -$padding;
-        padding-top: $navbarheight;
-        z-index: 2;
-        .project-list {
-            margin-right: -$padding;
-            margin-left: $padding * 2;
-            ul {
-                list-style: none;
-                padding: $padding;
-                background: #fff;
-                li {
-                    cursor: pointer;
-                    margin-bottom: 24px;
-                    line-height: 36px;
-                    margin-left: $padding / 2;
-                    h3,
-                    span {
-                        display: inline-block;
-                        vertical-align: bottom;
-                    }
-                    h3 {
-                        font-size: 24px;
-                        font-weight: 600;
-                        color: $textcolor2;
-                        transition: all $anim / 2 $ease-out-quint;
-                    }
-                    span {
-                        color: $primary;
-                        font-size: 14px;
-                        line-height: 36px;
-                    }
-                    &:hover,
-                    &.active {
-                        h3 {
-                            font-size: 32px;
-                            color: $primary;
-                        }
-                    }
-                    &:last-child {
-                        margin-bottom: 0;
-                    }
-                }
-            }
-        }
+      margin: 0;
+      background: transparent;
+      .project-list {
+        margin: $padding;
+        margin-top: 0;
+      }
     }
     .right {
-        z-index: 1;
-        img {
-            cursor: pointer;
-            min-width: 100%;
-            &.expand {
-                height: 100vh;
-            }
-        }
+      position: fixed;
+      top: 0;
     }
     .info-block {
-        background: $primary;
-        padding: $padding / 2;
-        position: absolute;
-        bottom: $padding / 2;
-        left: 90%;
-        min-width: 200px;
-        width: 320px;
-        p {
-            color: #fff;
-            font-size: 14px;
-        }
-        .under {
-            color: #fff;
-            &::before {
-                background: #fff;
-            }
-            &::after {
-                background: $primary;
-            }
-        }
+      right: $padding / 2;
+      left: inherit;
+      bottom: 10px;
     }
-    @media screen and (max-width: 960px) {
-        .left,
-        .right {
-            width: 100%;
-            display: block;
-        }
-        .left {
-            margin: 0;
-            background: transparent;
-            .project-list {
-                margin: $padding;
-                margin-top: 0;
+  }
+  @media screen and (max-width: 768px) {
+    .left {
+      .project-list {
+        margin: 0 ($padding / 2);
+        ul {
+          padding: $padding / 2;
+          text-align: center;
+          li {
+            margin-left: 0;
+            span {
+              width: 100%;
             }
+          }
         }
-        .right {
-            position: fixed;
-            top: 0;
-        }
-        .info-block {
-            right: $padding / 2;
-            left: inherit;
-            bottom: 10px;
-        }
+      }
+      .info-block {
+        position: relative;
+        left: 0;
+        right: 0;
+        margin: 0 auto;
+      }
     }
-    @media screen and (max-width: 768px) {
-        .left {
-            .project-list {
-                margin: 0 ($padding / 2);
-                ul {
-                    padding: $padding / 2;
-                    text-align: center;
-                    li {
-                        margin-left: 0;
-                        span {
-                            width: 100%;
-                        }
-                    }
-                }
-            }
-            .info-block {
-                position: relative;
-                left: 0;
-                right: 0;
-                margin: 0 auto;
-            }
-        }
-    }
+  }
 }
 </style>
