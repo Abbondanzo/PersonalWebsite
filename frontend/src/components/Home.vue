f4cf790c3e603b42e17c8508354819ff837b2ed8
unknown <peter@abbondanzo.com>
2020-03-29 15:04:47 -0400

    Format vue files

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -1,69 +1,69 @@
 <template>
-  <div class="content">
-    <div class="valign">
-      <div class="container">
-        <div class="hello-text">
-          <h1>Peter Abbondanzo</h1>
-          <h2>Software Developer</h2>
-          <router-link :to="{ path: 'about' }">
-            <button class="btn btn-white">About</button>
-          </router-link>
+    <div class="content">
+        <div class="valign">
+            <div class="container">
+                <div class="hello-text">
+                    <h1>Peter Abbondanzo</h1>
+                    <h2>Software Developer</h2>
+                    <router-link :to="{ path: 'about' }">
+                        <button class="btn btn-white">About</button>
+                    </router-link>
+                </div>
+            </div>
         </div>
-      </div>
     </div>
-  </div>
 </template>
 
 <script>
 export default {
-  name: 'Home',
-  mounted() {
-    console.log('Mounted Home component') // a.k.a. Working
-  }
+    name: 'Home',
+    mounted() {
+        console.log('Mounted Home component') // a.k.a. Working
+    }
 }
 </script>
 
 <style lang="scss" scoped>
 // @import 'global';
 #app {
-  .content {
-    height: 100vh;
-    a {
-      text-align: center;
-    }
-    .hello-text {
-      text-align: center;
-      // overflow: auto;
-      h1,
-      h2 {
-        color: #fff;
-        cursor: default;
-        pointer-events: none;
-      }
-      h1 {
-        font-size: 96px;
-        // white-space: nowrap;
-      }
-      h2 {
-        font-weight: 300;
-        font-size: 40px;
-        padding-bottom: 48px;
-      }
-    }
-  }
-  @media screen and (max-width: 1360px) {
     .content {
-      .hello-text {
-        h1 {
-          font-size: 40px;
-          white-space: normal;
+        height: 100vh;
+        a {
+            text-align: center;
         }
-        h2 {
-          font-size: 20px;
-          padding-bottom: 24px;
+        .hello-text {
+            text-align: center;
+            // overflow: auto;
+            h1,
+            h2 {
+                color: #fff;
+                cursor: default;
+                pointer-events: none;
+            }
+            h1 {
+                font-size: 96px;
+                // white-space: nowrap;
+            }
+            h2 {
+                font-weight: 300;
+                font-size: 40px;
+                padding-bottom: 48px;
+            }
         }
-      }
     }
-  }
+    @media screen and (max-width: 1360px) {
+        .content {
+            .hello-text {
+                h1 {
+                    font-size: 40px;
+                    white-space: normal;
+                }
+                h2 {
+                    font-size: 20px;
+                    padding-bottom: 24px;
+                }
+            }
+        }
+    }
 }
 </style>
