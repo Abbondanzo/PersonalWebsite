a3d34ef3e7e4661c684781e7395d60fbdd3935ca
Abbondanzo <peter@abbondanzo.com>
2022-04-16 15:06:53 -0400

    Fade all HTML

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -97,19 +97,6 @@ export default {
 </script>
 
 <style lang="scss">
-@keyframes fadeIn {
-  from {
-    opacity: 0;
-  }
-  to {
-    opacity: 1;
-  }
-}
-
-html {
-  animation: fadeIn 2s;
-}
-
 body {
   background: url('~@/assets/img/bg.jpg') top left no-repeat;
   background-size: cover;
